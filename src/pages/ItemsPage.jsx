38385e3a085b463275685e20543632355caddc41
kishorevic12 <23suca07@tcarts.in>
2026-03-23 18:16:07 +0530

    fix

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -63,7 +63,7 @@
 
       const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' })
       const url = URL.createObjectURL(blob)
-
+ 
       const link = document.createElement('a')
       link.href = url
       link.setAttribute('download', 'items.csv')
